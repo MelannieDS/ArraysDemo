1e09613e8af0ab20c0ed72b6b244043885690df0
MelannieDS <134092297+MelannieDS@users.noreply.github.com>
2023-05-26 12:43:41 -0400

    Update Student.java
    comment added for fetch and merge

--- src/arraysdemo/Student.java
+++ src/arraysdemo/Student.java
@@ -1,13 +1,11 @@
 
 package arraysdemo;
 
-
 public class Student {
     
     //comment added at 1:21 PM
     //adding new comment at 1:29PM
-    
-    
+  //adding comment for fetch and merge
     
     private String sID;
     private String sName;
